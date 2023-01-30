e4cd6c102d8dbd010cb381eb98fc3ac79b350bae
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-01-30 16:24:15 -0700

    :fire: Remove old assertions file

--- src/util/Assertions.ts
+++
@@ -1,4 +0,0 @@
-export function assertNotNullOrUndefined<T>(value: T | null | undefined, context?: string): asserts value is T {
-    expect(value).withContext(context ?? "").not.toBeNull();
-    expect(value).withContext(context ?? "").not.toBeUndefined();
-}
